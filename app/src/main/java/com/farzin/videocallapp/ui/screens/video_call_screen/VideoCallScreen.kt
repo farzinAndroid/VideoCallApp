b2475de8099ee5150f5c64f41b990bf0f612dbe7
Farzin <farzinallahyari1234@gmail.com>
2024-09-07 10:54:17 +0330

    VideoCallScreen

--- app/src/main/java/com/farzin/videocallapp/ui/screens/video_call_screen/VideoCallScreen.kt
+++ app/src/main/java/com/farzin/videocallapp/ui/screens/video_call_screen/VideoCallScreen.kt
@@ -1,16 +1,76 @@
 package com.farzin.videocallapp.ui.screens.video_call_screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import org.webrtc.SurfaceViewRenderer
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VideoCallScreen(
     navController: NavController,
-    roomId:String
+    roomId: String,
 ) {
 
-    Text(text = "video call : $roomId")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        text = "Video Call",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            )
+        },
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                AndroidView(
+                    factory = { context ->
+                        SurfaceViewRenderer(context).apply {
+
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                )
+
+                AndroidView(
+                    factory = { context ->
+                        SurfaceViewRenderer(context).apply {
+
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                )
+            }
+        }
+    )
 
 }
